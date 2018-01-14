bf45b15c8c429d35fc1b2a019dca90213e3326ad
ionush <neilasjoseph@gmail.com>
2018-01-14 18:25:52 +0200

    copying bug to repo

---
+++ js/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { hot } from 'react-hot-loader'; // eslint-disable-line
+
+const App = () => (
+  <div>
+    <h1>watch this fdmarlen</h1>
+  </div>
+);
+
+export default hot(module)(App);
